450af59495864814b90d15f91dfc5e276462a14f
EduardoPulcino <eduardo.pulcino@sptech.school>
2024-09-22 17:19:14 -0300

    Adiciona lombok nas classes

--- src/main/java/buffet/app_web/entities/Decoracao.java
+++ src/main/java/buffet/app_web/entities/Decoracao.java
@@ -5,7 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class Decoracao {
     @Id
@@ -14,36 +18,4 @@ public class Decoracao {
     private TipoEvento tipoEvento;
     private String nome;
     private String foto;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public TipoEvento getTipoEvento() {
-        return tipoEvento;
-    }
-
-    public void setTipoEvento(TipoEvento tipoEvento) {
-        this.tipoEvento = tipoEvento;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getFoto() {
-        return foto;
-    }
-
-    public void setFoto(String foto) {
-        this.foto = foto;
-    }
 }
