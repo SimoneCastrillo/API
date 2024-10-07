90b4f4fa393a82fbb3b57f1c60728f54c8ee3d06
FernandaFlores2 <142940761+FernandaFlores2@users.noreply.github.com>
2024-10-07 00:13:34 -0300

    Implementando Swagger (#14)

--- src/main/java/buffet/app_web/controllers/DecoracaoController.java
+++ src/main/java/buffet/app_web/controllers/DecoracaoController.java
@@ -6,6 +6,13 @@ import buffet.app_web.entities.Decoracao;
 import buffet.app_web.entities.TipoEvento;
 import buffet.app_web.mapper.DecoracaoMapper;
 import buffet.app_web.strategies.DecoracaoStrategy;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +24,27 @@ import static org.springframework.http.ResponseEntity.*;
 
 @RestController
 @RequestMapping("/decoracoes")
+@Tag(name = "Decorações", description = "Operações relacionadas a decorações")
 public class DecoracaoController {
     @Autowired
     private DecoracaoStrategy decoracaoStrategy;
 
+    @Operation(summary = "Listar todas as decorações", description = """
+            # Listar todas as decorações
+            ---
+            O endpoint lista todas as decorações cadastradas.
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista todas as decorações",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Decoracao.class))
+                    )
+            ),
+            @ApiResponse(responseCode = "204", description = "Quando não tem decorações cadastradas",
+                    content = @Content()
+            )
+    })
     @GetMapping
     public ResponseEntity<List<DecoracaoResponseDto>> listarTodos(){
         if (decoracaoStrategy.listarTodos().isEmpty()){
@@ -32,11 +56,40 @@ public class DecoracaoController {
         return ok(listaDto);
     }
 
+    @Operation(summary = "Buscar uma decoração por ID", description = """
+            # Buscar decoração
+            ---
+            Retorna a decoração correspondente ao ID fornecido.
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Decoração encontrada",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Decoracao.class)
+                    )
+            ),
+            @ApiResponse(responseCode = "404", description = "Decoração não encontrada",
+                    content = @Content()
+            )
+    })
     @GetMapping("/{id}")
     public ResponseEntity<DecoracaoResponseDto> buscarPorId(@PathVariable int id){
         return ok(DecoracaoMapper.toResponseDto(decoracaoStrategy.buscarPorId(id)));
     }
 
+    @Operation(summary = "Criar uma nova decoração", description = """
+            # Criar decoração
+            ---
+            Cria e retorna a nova decoração.
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Decoração criada",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Decoracao.class)
+                    )
+            )
+    })
     @PostMapping
     public ResponseEntity<DecoracaoResponseDto> publicar(@RequestBody @Valid DecoracaoRequestDto decoracaoRequestDto){
         Decoracao decoracao = decoracaoStrategy.salvar(DecoracaoMapper.toEntity(decoracaoRequestDto));
@@ -44,6 +97,22 @@ public class DecoracaoController {
         return ok(decoracaoResponseDto);
     }
 
+    @Operation(summary = "Atualizar uma decoração", description = """
+            # Atualizar decoração
+            ---
+            Atualiza a decoração correspondente ao ID fornecido.
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Decoração atualizada",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Decoracao.class)
+                    )
+            ),
+            @ApiResponse(responseCode = "404", description = "Decoração não encontrada",
+                    content = @Content()
+            )
+    })
     @PutMapping("/{id}")
     public ResponseEntity<DecoracaoResponseDto> atualizar(@RequestBody @Valid DecoracaoRequestDto decoracaoRequestDto, @PathVariable int id){
         decoracaoStrategy.buscarPorId(id);
@@ -55,6 +124,19 @@ public class DecoracaoController {
         return ok(DecoracaoMapper.toResponseDto(decoracaoSalva));
     }
 
+    @Operation(summary = "Deletar uma decoração", description = """
+            # Deletar decoração
+            ---
+            Deleta a decoração correspondente ao ID fornecido.
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Decoração deletada",
+                    content = @Content()
+            ),
+            @ApiResponse(responseCode = "404", description = "Decoração não encontrada",
+                    content = @Content()
+            )
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable int id){
         decoracaoStrategy.buscarPorId(id);
