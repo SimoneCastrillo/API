77d6284c48db6ede65a34ff15522758da4ee95b9
FernandaFlores2 <fernanda.flores@sptech.school>
2024-10-20 18:59:33 -0300

    Implementa JWT

--- src/main/java/buffet/app_web/service/UsuarioService.java
+++ src/main/java/buffet/app_web/service/UsuarioService.java
@@ -1,11 +1,20 @@
 package buffet.app_web.service;
 
+import buffet.app_web.configuration.security.jwt.GerenciadorTokenJwt;
 import buffet.app_web.entities.Usuario;
+import buffet.app_web.mapper.UsuarioMapper;
 import buffet.app_web.repositories.UsuarioRepository;
+import buffet.app_web.service.autenticacao.dto.UsuarioLoginDto;
+import buffet.app_web.service.autenticacao.dto.UsuarioTokenDto;
 import buffet.app_web.strategies.UsuarioStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -14,9 +23,19 @@ import java.util.Optional;
 
 @Service
 public class UsuarioService implements UsuarioStrategy {
-    @Autowired
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private GerenciadorTokenJwt gerenciadorTokenJwt;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
 
     public List<Usuario> listarTodos(){
         return  usuarioRepository.findAll();
@@ -27,6 +46,8 @@ public class UsuarioService implements UsuarioStrategy {
     }
 
     public Usuario salvar(Usuario usuario){
+        String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(senhaCriptografada);
         return  usuarioRepository.save(usuario);
     }
 
@@ -35,5 +56,24 @@ public class UsuarioService implements UsuarioStrategy {
         usuarioRepository.deleteById(id);
     }
 
+    public UsuarioTokenDto autenticar(UsuarioLoginDto usuarioLoginDto) {
+
+        final UsernamePasswordAuthenticationToken credentials = new UsernamePasswordAuthenticationToken(
+                usuarioLoginDto.getEmail(), usuarioLoginDto.getSenha());
+
+        final Authentication authentication = this.authenticationManager.authenticate(credentials);
+
+        Usuario usuarioAutenticado =
+                usuarioRepository.findByEmail(usuarioLoginDto.getEmail())
+                        .orElseThrow(
+                                () -> new ResponseStatusException(404, "Email do usuário não cadastrado", null)
+                        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        final String token = gerenciadorTokenJwt.generateToken(authentication);
+
+        return UsuarioMapper.of(usuarioAutenticado, token);
+    }
 
 }
